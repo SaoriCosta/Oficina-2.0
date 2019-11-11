eb16c96f7e031a6d717118e68f48be2223858b34
Saori <saoricosta@gmail.com>
2019-11-10 22:51:01 -0300

    Correcoes

--- src/Repository/OrcamentoRepository.php
+++ src/Repository/OrcamentoRepository.php
@@ -126,33 +126,4 @@ class OrcamentoRepository extends ServiceEntityRepository
 
     }
 
-
-    // /**
-    //  * @return Orcamento[] Returns an array of Orcamento objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('o')
-            ->andWhere('o.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('o.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Orcamento
-    {
-        return $this->createQueryBuilder('o')
-            ->andWhere('o.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
